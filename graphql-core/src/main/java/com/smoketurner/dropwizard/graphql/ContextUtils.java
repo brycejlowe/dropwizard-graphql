3657e8c70b65d7d0a3e210d5dc2a64a00c33689f
Bryce Lowe <brycel@zillowgroup.com>
2022-04-24 21:45:00 -0700

    Publish 1.3.8-Z2

---
+++ graphql-core/src/main/java/com/smoketurner/dropwizard/graphql/ContextUtils.java
@@ -0,0 +1,23 @@
+package com.smoketurner.dropwizard.graphql;
+
+import graphql.kickstart.servlet.context.DefaultGraphQLServletContext;
+import graphql.schema.DataFetchingEnvironment;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ContextUtils {
+    public static HttpServletRequest getRequest(DataFetchingEnvironment dfe) {
+        // TODO: Fix once 13.0.0 drops (https://github.com/graphql-java-kickstart/graphql-java-servlet/issues/393)
+        DefaultGraphQLServletContext servletContext = dfe.getContext();
+        return servletContext.getHttpServletRequest();
+    }
+
+    public static String getHeader(DataFetchingEnvironment dfe, String headerName) {
+        return getRequest(dfe).getHeader(headerName);
+    }
+
+    public String getHeader(DataFetchingEnvironment dfe, String headerName, String defaultValue) {
+        String headerValue = getHeader(dfe, headerName);
+        return headerValue != null ? headerValue : defaultValue;
+    }
+}
